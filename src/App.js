93ee1dcb241aad50ce92f6cc9990fadb546a5c04
Ostin22 <rrcarrion@puce.edu.ec>
2025-01-25 13:23:11 -0500

    agregar nombres

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Creado por Ricardo Carrión y Jhosue Lopez.
         </p>
         <a
           className="App-link"
